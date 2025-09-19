fc94dc52cbc18d24ef5a47a830f3e748021a14f8
kiran2004-code <135125274+kiran2004-code@users.noreply.github.com>
2025-09-19 22:49:50 +0530

    Add files via upload

---
+++ Exception/InsufficientValues.java
@@ -0,0 +1,8 @@
+package Exception;
+
+public class InsufficientValues extends RuntimeException {
+    public InsufficientValues(String message) {
+
+        super(message);
+    }
+}
---
+++ Exception/StackWasEmpty.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class StackWasEmpty extends RuntimeException {
+  public StackWasEmpty(String message) {
+    super(message);
+  }
+}
